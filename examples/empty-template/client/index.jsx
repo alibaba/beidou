03f16b7e5cf69956e7c69badf9e7ac2d678bd0a5
weichunpeng <holdenwei@qq.com>
2017-07-20 09:17:06 +0800

    fix: typo

--- examples/empty-template/client/index.jsx
+++ examples/empty-template/client/index.jsx
@@ -17,7 +17,7 @@ export default class Index extends React.Component {
     return (
       <div>
         <h1 style={titleStyle} >Hello World, Beidou</h1>
-        <div style={textStyle} >you might need
+        <div style={textStyle} >you might need 
           <a href="https://github.com/alibaba/beidou" target="_blank" rel="noopener noreferrer" >Beidou document</a>
         </div>
       </div>
