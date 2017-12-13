66f8b40943cfd7f67180d59505b091279f1c885e
Gray <njugray@gmail.com>
2017-12-13 10:59:04 +0800

    update examples

--- examples/redux/config/config.default.js
+++ examples/redux/config/config.default.js
@@ -5,9 +5,6 @@ const path = require('path');
 module.exports = {
   keys: 'key',
   isomorphic: {
-    babel: {
-      plugins: ['add-module-exports', 'react-hot-loader/babel'],
-    },
     universal: {
       assetsFilePath: path.join(__dirname, '../.isomorphic/assets.json'),
       assets: ['.scss', '.png'],
