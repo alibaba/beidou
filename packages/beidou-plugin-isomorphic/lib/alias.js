82257e32936d01b3d350e0990a601b0332c83a00
Gray <njugray@gmail.com>
2017-12-19 16:48:53 +0800

    feat: server side resolve alias

---
+++ packages/beidou-plugin-isomorphic/lib/alias.js
@@ -0,0 +1,16 @@
+const Module = require('module');
+const path = require('path');
+
+module.exports = function handle(config) {
+  const originalResolveFileName = Module._resolveFilename;
+  const keys = Object.keys(config);
+  Module._resolveFilename = function (request, parent) {
+    for (let i = 0; i < keys.length; i += 1) {
+      if (request === keys[i] || request.indexOf(`${keys[i]}/`) === 0) {
+        request = path.normalize(`${config[keys[i]]}${request.substring(keys[i].length)}`);
+        break;
+      }
+    }
+    return originalResolveFileName.apply(this, [request, parent]);
+  };
+};
