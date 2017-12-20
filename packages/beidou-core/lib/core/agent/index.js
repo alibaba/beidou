a5576e10a962a4ad705240ba434b4f60b7fed3ec
Gray <njugray@gmail.com>
2017-12-20 16:08:33 +0800

    feat: remove config dump override

--- packages/beidou-core/lib/core/agent/index.js
+++ packages/beidou-core/lib/core/agent/index.js
@@ -24,24 +24,6 @@ class BeidouAgent extends egg.Agent {
   get [Symbol.for('egg#loader')]() {
     return AgentWorkerLoader;
   }
-
-  /**
-   * 将 app.config 保存到 debug/${type}_config.json
-   */
-  dumpConfig() {
-    const rundir = (this.config && this.config.rundir) || 'run';
-    const configdir = path.join(rundir, `${this.type}_${new Date().toString()}_config.json`);
-    try {
-      fs.existsSync(rundir) || fs.mkdirSync(rundir);
-      fs.writeFileSync(configdir, JSON.stringify({
-        config: this.config,
-        plugins: this.plugins,
-      }, null, 2));
-    } catch (err) {
-      /* istanbul ignore next */
-      this.logger.warn(`duplicateConfig error: ${err.message} ${err.stack ? err.stack : ''}`);
-    }
-  }
 }
 
 module.exports = BeidouAgent;
