73a28a1347673d137b25927cf3254d25b3b15123
Gray <njugray@gmail.com>
2018-01-10 20:18:19 +0800

    fix(isomorphic): default config of js ext

--- packages/beidou-plugin-isomorphic/config/config.default.js
+++ packages/beidou-plugin-isomorphic/config/config.default.js
@@ -38,7 +38,7 @@ module.exports = function (app) {
 
       // Setting this will remove the currently hooked extensions of `.es6`, `.es`, `.jsx`, `.mjs`
       // and .js so you'll have to add them back if you want them to be used again.
-      extensions: ['.es6', '.es', '.jsx', '.mjs'],
+      extensions: ['.es6', '.es', '.js', '.jsx', '.mjs'],
 
       // Setting this to false will disable the cache.
       cache: true,
