a0b54a00c9e8a04096cfa28c07dcc143a9cf2af5
Gray <njugray@gmail.com>
2018-02-08 10:35:30 +0800

    feat(beidou-core): refactor framework inheritance strategy (#28)

--- packages/beidou-core/lib/core/loaders/app-worker-loader.js
+++ packages/beidou-core/lib/core/loaders/app-worker-loader.js
@@ -1,77 +1,73 @@
 'use strict';
 
 const path = require('path');
-const egg = require('egg');
 
-/**
- * App Worker Loader, extend Egg.AppWorkerLoader
- * @extends Egg.AppWorkerLoader
- */
-class BeidouAppWorkerLoader extends egg.AppWorkerLoader {
-  constructor(options) {
-    super(options);
+module.exports = function (target) {
+  /**
+   * App Worker 进程的 Loader，继承 Egg.AppWorkerLoader
+   * @extends Egg.AppWorkerLoader
+   */
+  class BeidouAppWorkerLoader extends target.AppWorkerLoader {
+    constructor(options) {
+      super(options);
 
-    this.logger =
-      options.logger ||
-      /* istanbul ignore next */ options.console ||
-      /* istanbul ignore next */ console;
+      // 使用egg提供的console, 能够统一控制日志输出级别
+      this.logger = options.logger || /* istanbul ignore next */ options.console || /* istanbul ignore next */ console;
 
-    this.logger.info(
-      '[Beidou Loader] Beidou Loader started, pid is %s',
-      process.pid
-    );
-  }
+      this.logger.info('[Beidou Loader] Beidou Loader started, pid is %s', process.pid);
+    }
 
-  /*
+    /*
    * custom loadRouter
    * @method EggLoader#loadRouter
    */
-  loadRouter() {
-    super.loadRouter();
-    this.loadCustomRouter();
-  }
-
-  loadCustomRouter() {
-    for (const unit of this.getLoadUnits()) {
-      this.loadFile(path.join(unit.path, 'app/extend/router.js'));
+    loadRouter() {
+      super.loadRouter();
+      this.loadCustomRouter();
     }
-  }
 
-  /*
+    loadCustomRouter() {
+      this.getLoadUnits()
+        .forEach(unit => this.loadFile(path.join(unit.path, 'app/extend/router.js')));
+    }
+
+    /*
    * custom loadConfig
    * @method EggLoader#loadConfig
    */
-  loadConfig() {
-    super.loadConfig();
+    loadConfig() {
+      super.loadConfig();
 
-    this.loadAssetsInfo();
-  }
+      // 预留扩展
+      this.loadAssetsInfo();
+    }
 
-  /**
+    /**
    * @deprecated
    * load static resources based on CDN root.
    * eg. /cm/store-decoration-m/0.0.2/
    */
-  loadAssetsInfo() {
-    let assetsInfo = this.config.assetsInfo;
-    if (!assetsInfo) {
-      this.config.assetsInfo = assetsInfo = {};
-    }
+    loadAssetsInfo() {
+      let assetsInfo = this.config.assetsInfo;
+      if (!assetsInfo) {
+        this.config.assetsInfo = assetsInfo = {};
+      }
 
-    let group = assetsInfo.group;
-    if (!group) {
-      group = '';
+      let group = assetsInfo.group;
+      if (!group) {
+        group = '';
+      }
+      let project = assetsInfo.project;
+      if (!project) {
+        project = this.pkg.name;
+      }
+      let version = assetsInfo.version;
+      if (!version) {
+        version = this.pkg.version;
+      }
+      this.config.assetsInfo.cdnRoot = `/${group}/${project}/${version}/`;
     }
-    let project = assetsInfo.project;
-    if (!project) {
-      project = this.pkg.name;
-    }
-    let version = assetsInfo.version;
-    if (!version) {
-      version = this.pkg.version;
-    }
-    this.config.assetsInfo.cdnRoot = `/${group}/${project}/${version}/`;
   }
-}
 
-module.exports = BeidouAppWorkerLoader;
+  target.AppWorkerLoader = BeidouAppWorkerLoader;
+};
