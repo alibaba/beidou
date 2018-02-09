96eff77f0f1d589622ab8f7df09e41c9db428d82
Gray <njugray@gmail.com>
2018-02-09 22:55:00 +0800

    chore: remove unused packages

--- packages/beidou-core/lib/core/loaders/index.js
+++ packages/beidou-core/lib/core/loaders/index.js
@@ -5,9 +5,7 @@
  */
 
 const appWorkerLoaderExtend = require('./app-worker-loader');
-const agentWorkerLoaderExtend = require('./agent-worker-loader');
 
 module.exports = function (target) {
   appWorkerLoaderExtend(target);
-  agentWorkerLoaderExtend(target);
 };
--- packages/beidou-core/test/fixtures/apps/agent-check-devdependencies/config/config.default.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = antx => {
-  return {
-    debugDir: `${antx.baseDir}/debug`,
-    keys: '123456'
-  }
-}
