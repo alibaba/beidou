571aa7692911c0c9f6798690dcdcea1e7b841e85
Gray <njugray@gmail.com>
2018-02-12 11:36:41 +0800

    feat: standard args for build

--- packages/beidou-core/bin/build.js
+++ packages/beidou-core/bin/build.js
@@ -3,6 +3,7 @@
 'use strict';
 
 const process = require('process');
+const argv = require('argh').argv;
 const fs = require('fs');
 const path = require('path');
 const { Application } = require('../index');
@@ -16,18 +17,19 @@ const app = new Application({
   workers: 1,
 });
 
-// build in production environment
-app.config.env = 'prod';
+// build in production environment as default
+app.config.env = argv.dev ? 'local' : 'prod';
 
-const execEnv = process.argv[2];
-if (execEnv && !['node', 'browser'].includes(execEnv)) {
+const target = argv.target;
+
+if (target && !['node', 'browser'].includes(target)) {
   app.coreLogger.error(
-    `Expect execute environment to be "node" or "browser"(default), got ${execEnv}`
+    `Expect execute environment to be "node" or "browser"(default), got ${target}`
   );
   process.exit(1);
 }
 
-const compiler = builder(app, execEnv);
+const compiler = builder(app, target);
 
 compiler.run((err, stats) => {
   if (err) {
