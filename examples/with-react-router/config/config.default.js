2bca7ac60624210a5dd9af7e1f767bcebb2ebdb8
devrsi0n <devrsi0n@gmail.com>
2018-03-07 20:44:53 +0800

    feat(beidou-cli): print version before run command

--- examples/with-react-router/config/config.default.js
+++ examples/with-react-router/config/config.default.js
@@ -1,18 +1,5 @@
 'use strict';
 
-const path = require('path');
-
 module.exports = {
   keys: 'secret',
-  webpack: {
-    resolve: {
-      alias: {
-        client: path.join(__dirname, '../client'),
-      },
-    },
-  },
-
-  react: {
-    assetPath: '/build',
-  },
 };
