bc42dd228a3b2eb5263a95ee157c6a7d905e090c
Gray <njugray@gmail.com>
2018-03-26 12:33:55 +0800

    fix: prevent `lib/core/agent/index.js` unexpectly loaded by plugin loader

--- packages/beidou-core/lib/beidou.js
+++ packages/beidou-core/lib/beidou.js
@@ -8,8 +8,8 @@ const path = require('path');
 
 const EGG_PATH = path.join(__dirname, '..');
 const loaderExtend = require('./core/loaders');
-const applicationExtend = require('./core/worker/index');
-const agentExtend = require('./core/agent/index');
+const applicationExtend = require('./core/worker/application');
+const agentExtend = require('./core/worker/agent');
 
 module.exports = function (target) {
   loaderExtend(target);
