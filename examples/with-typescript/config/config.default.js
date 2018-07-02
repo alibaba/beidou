9ee0149a5158697fd01c463b3f07df370fb51bd8
Gray <njugray@gmail.com>
2018-07-02 22:25:37 +0800

    examples: typescript

---
+++ examples/with-typescript/config/config.default.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+  keys: 'secret',
+  view: {
+    defaultExtension: '.tsx',
+  },
+  router: {
+    exts: ['.jsx', '.ts', '.tsx'],
+  },
+  webpack: {
+    // your webpack config file
+    custom: {
+      configPath: path.resolve(__dirname, './webpack.config.js'),
+    },
+    resolve: {
+      extensions: ['.json', '.js', '.jsx', '.ts', '.tsx'],
+      alias: {
+        client: path.join(__dirname, '../client'),
+      },
+    },
+  },
+};
