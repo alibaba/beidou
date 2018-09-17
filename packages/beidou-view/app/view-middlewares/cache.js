b002cabc202f63437bda32fa31023dbca53ac874
陈祥 <1447388624@qq.com>
2018-09-17 14:48:01 +0800

    fix:缓存逻辑在win下面的bug修复 (#101)
     fix #71

--- packages/beidou-view/app/view-middlewares/cache.js
+++ packages/beidou-view/app/view-middlewares/cache.js
@@ -1,9 +1,10 @@
 'use strict';
+const escapeStringRegexp = require('escape-string-regexp');
 
 module.exports = async function (viewCtx, next) {
   // clean cache
   if (!viewCtx.options.cache) {
-    const roots = viewCtx.config.view.root;
+    const roots = viewCtx.config.view.root.map(item => escapeStringRegexp(item));
     const regexp = new RegExp(`(${roots.join('|')})`);
     for (const mod of Object.keys(require.cache)) {
       if (regexp.test(require.cache[mod].filename)) {
