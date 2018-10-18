b70395e403d6b8050122b88069263dcca329f73b
Gray <njugray@gmail.com>
2018-10-18 10:26:22 +0800

    fix: babel config combination

--- packages/beidou-isomorphic/app.js
+++ packages/beidou-isomorphic/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const path = require('path');
+const fs = require('fs');
 const { basicPolyfill } = require('./lib/polyfill');
 const isomorphic = require('./lib/isomorphic');
 
@@ -17,20 +19,42 @@ function setGlobal(ENV) {
 }
 
 module.exports = (app) => {
-  const config = app.config.isomorphic;
-  // set global variables
-  setGlobal(app.config.env || /* istanbul ignore next */ app.loader.serverEnv);
+  app.beforeStart(() => {
+    const config = app.config.isomorphic;
+    // set global variables
+    setGlobal(
+      app.config.env || /* istanbul ignore next */ app.loader.serverEnv
+    );
 
-  // jsdom polyfill, enabled by default
-  config.polyfill && basicPolyfill();
+    // jsdom polyfill, enabled by default
+    config.polyfill && basicPolyfill();
 
-  // babel-register
-  const { babel } = config;
+    // babel-register
+    const { babel } = config;
 
-  if (babel) {
-    require('@babel/register')(babel);
-  }
+    const babelrcConfigFile = path.join(app.baseDir, '.node.babelrc');
+    const babelrcJSFile = path.join(app.baseDir, '.node.babelrc.js');
+    let customConfig = null;
+    // try to load server-side babelrc file named `.node.babelrc` or `.node.babelrc.js`
+    if (fs.existsSync(babelrcConfigFile)) {
+      const raw = fs.readFileSync(babelrcConfigFile);
+      customConfig = JSON.parse(raw);
+    }
 
-  // isomorphic register
-  isomorphic(app);
+    if (fs.existsSync(babelrcJSFile)) {
+      customConfig = require(babelrcJSFile);
+    }
+
+    if (babel) {
+      let final = babel;
+      if (customConfig) {
+        final = Object.assign(babel, customConfig);
+      }
+
+      require('@babel/register')(final);
+    }
+
+    // isomorphic register
+    isomorphic(app);
+  });
 };
