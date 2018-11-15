79f2906da446c0d4491daae8ab6bb0b70f1faad6
dxd_sjtu <dxd_sjtu@outlook.com>
2018-11-15 20:50:53 +0800

    chore: remove stage2 for @babel/corev7.0.0-beta.55+

--- examples/with-typescript/app.js
+++ examples/with-typescript/app.js
@@ -8,11 +8,21 @@ require('@babel/register')({
         },
       },
     ],
-    ['@babel/preset-stage-2', { decoratorsLegacy: true }],
     '@babel/react',
     '@babel/typescript',
   ],
   plugins: [
+    // Stage 2
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    '@babel/plugin-proposal-function-sent',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-numeric-separator',
+    '@babel/plugin-proposal-throw-expressions',
+    // Stage 3
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-syntax-import-meta',
+    '@babel/plugin-proposal-json-strings',
+    // other
     '@babel/plugin-transform-modules-commonjs',
     '@babel/proposal-class-properties',
     '@babel/proposal-object-rest-spread',
--- examples/with-typescript/config/webpack.config.js
+++ examples/with-typescript/config/webpack.config.js
@@ -1,10 +1,9 @@
 'use strict';
 
 const webpack = require('webpack');
-// const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = (app, defaultConfig, dev) => {
-  if (dev) {
+  if (app && dev) {
     defaultConfig.plugins.push(
       new webpack.DefinePlugin({
         __ENV__: JSON.stringify('daily'),
@@ -36,92 +35,27 @@ module.exports = (app, defaultConfig, dev) => {
     exclude: /node_modules/,
     use: [
       {
-        loader: 'babel-loader',
+        loader: require.resolve('babel-loader'),
         options: {
           babelrc: false,
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                targets: {
-                  browsers: ['>1%', 'last 4 versions', 'not ie < 9'],
-                },
-                useBuiltIns: 'entry',
-                // Do not transform modules to CJS
-                modules: false,
-              },
-            ],
-            '@babel/typescript',
-            ['@babel/preset-stage-2', { decoratorsLegacy: true }],
-            [
-              '@babel/preset-react',
-              {
-                development: dev,
-                useBuiltIns: true,
-              },
-            ],
-          ],
-
-          plugins: [
-            [
-              '@babel/transform-runtime',
-              {
-                polyfill: false,
-                regenerator: true,
-              },
-            ],
-          ],
-          env: {
-            development: {
-              plugins: ['module:react-hot-loader/babel'],
-            },
-          },
+          presets: [require.resolve('babel-preset-beidou-client')],
+          // This is a feature of `babel-loader` for webpack (not Babel itself).
+          // It enables caching results in ./node_modules/.cache/babel-loader/
+          // directory for faster rebuilds.
           cacheDirectory: dev,
           compact: !dev,
           highlightCode: true,
         },
       },
+      {
+        loader: require.resolve('ts-loader'),
+        options: {
+          transpileOnly: true,
+        },
+      },
     ],
   };
 
-  defaultConfig.module.rules[0].use = {
-    loader: 'babel-loader',
-    options: {
-      babelrc: false,
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: {
-              browsers: ['>1%', 'last 4 versions', 'not ie < 9'],
-            },
-            useBuiltIns: 'entry',
-            // Do not transform modules to CJS
-            modules: false,
-          },
-        ],
-        '@babel/preset-stage-2',
-        [
-          '@babel/preset-react',
-          {
-            development: dev,
-            useBuiltIns: true,
-          },
-        ],
-      ],
-      plugins: [],
-      env: {
-        development: {
-          plugins: ['module:react-hot-loader/babel'],
-        },
-      },
-      cacheDirectory: dev,
-      compact: !dev,
-      highlightCode: true,
-    },
-  };
-
   defaultConfig.module.rules.push(tsLoader);
-
   return defaultConfig;
 };
