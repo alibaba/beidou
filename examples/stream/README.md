182d279396bc702925035e591012b25b759101d2
Gray <njugray@gmail.com>
2018-12-13 17:29:07 +0800

    feat: support render to stream  (#126)
    * feat: support render to stream #124
    
    * examples: stream render
    
    * docs(view-react): add stream render doc

---
+++ examples/stream/README.md
@@ -0,0 +1,27 @@
+# Beidou Simple Example
+
+=================
+
+## Install
+
+```bash
+$ yarn install
+```
+
+## Start
+
+```bash
+$ npm start
+```
+
+then navigate to [http://127.0.0.1:6001/](http://127.0.0.1:6001/)
+
+## Basic Usage
+
+- add a new page in /client folder, eg. `/client/test.jsx`
+- start app: `npm start`
+- navigate to [http://127.0.0.1:6001/test](http://127.0.0.1:6001/test)
+
+## License
+
+[MIT](LICENSE)
---
+++ examples/stream/client/timing.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+/* eslint-disable no-alert */
+
+class Timing extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      TTFB: null,
+      contentDownload: null,
+      total: null,
+    };
+  }
+
+  componentDidMount() {
+    if (window.performance && window.performance.timing) {
+      const { timing } = window.performance;
+      const { requestStart, responseEnd, responseStart } = timing;
+      const TTFB = responseStart - requestStart;
+      const contentDownload = responseEnd - responseStart;
+      const total = responseEnd - requestStart;
+      this.setState({
+        TTFB,
+        contentDownload,
+        total,
+      });
+    }
+  }
+
+  render() {
+    const { TTFB, contentDownload, total } = this.state;
+    return (
+      <div>
+        <p>Use <a href="/">renderToString()</a></p>
+        <p>Use <a href="/?stream=1">renderToNodeStream()</a></p>
+        <h3>Timing</h3>
+        {
+          TTFB &&
+          <div style={{ backgroundColor: '#f2f2f2' }}>
+            <p>TTFB: {TTFB} ms</p>
+            <p>Content Download: {contentDownload} ms</p>
+            <p>Total: {total} ms</p>
+          </div>
+        }
+
+      </div>
+    );
+  }
+}
+
+export default Timing;
