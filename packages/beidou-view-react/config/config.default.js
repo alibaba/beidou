c43e8f0ecdd541b80ca15285702970b89307cdb1
Gray <njugray@gmail.com>
2019-01-11 14:33:51 +0800

    feat: auto detect view extension

--- packages/beidou-view-react/config/config.default.js
+++ packages/beidou-view-react/config/config.default.js
@@ -2,37 +2,41 @@
 
 const path = require('path');
 
-module.exports = appInfo => ({
-  /**
-   * React view options
-   * @member Config#react
-   */
-  react: {
-    middlewares: [
-      'cache',
-      'initialprops',
-      'redux',
-      'partial',
-      'render',
-      'beautify',
-      'doctype',
-    ],
-    beautify: false,
-    cache: true,
-    // Use `renderToStaticMarkup` if true, otherwise `renderToString`
-    static: false,
-    doctype: '<!DOCTYPE html>',
-    assetHost: '',
-    assetPath: '/build/',
-    // make sure the string pattern won't be used your you project.
-    placeHolder: '<!--$render$-->',
-  },
-  view: {
-    defaultViewEngine: 'react',
-    defaultExtension: '.jsx',
-    root: `${path.join(appInfo.baseDir, 'app/views')},${path.join(
-      appInfo.baseDir,
-      'client'
-    )}`,
-  },
-});
+module.exports = (appInfo) => {
+  const isTs = process.env.EGG_TYPESCRIPT === 'true';
+
+  return {
+    /**
+     * React view options
+     * @member Config#react
+     */
+    react: {
+      middlewares: [
+        'cache',
+        'initialprops',
+        'redux',
+        'partial',
+        'render',
+        'beautify',
+        'doctype',
+      ],
+      beautify: false,
+      cache: true,
+      // Use `renderToStaticMarkup` if true, otherwise `renderToString`
+      static: false,
+      doctype: '<!DOCTYPE html>',
+      assetHost: '',
+      assetPath: '/build/',
+      // make sure the string pattern won't be used your you project.
+      placeHolder: '<!--$render$-->',
+    },
+    view: {
+      defaultViewEngine: 'react',
+      defaultExtension: isTs ? '.tsx' : '.jsx',
+      root: `${path.join(appInfo.baseDir, 'app/views')},${path.join(
+        appInfo.baseDir,
+        'client'
+      )}`,
+    },
+  };
+};
