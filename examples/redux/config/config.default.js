f93a2b9f6db1779cd19cc7bd80718f676b47f7b3
dxd_sjtu <dxd_sjtu@outlook.com>
2019-02-27 14:41:23 +0800

    example: add build static folder

--- examples/redux/config/config.default.js
+++ examples/redux/config/config.default.js
@@ -1,8 +1,22 @@
 'use strict';
 
-module.exports = {
+const path = require('path');
+
+module.exports = appInfo => ({
   keys: 'secret',
   router: {
     entry: 'page',
   },
-};
+  static: {
+    dir: [
+      {
+        prefix: '/public',
+        dir: path.join(appInfo.baseDir, '/app/public'),
+      },
+      {
+        prefix: '/build',
+        dir: path.join(appInfo.baseDir, '/build'),
+      },
+    ],
+  },
+});
--- examples/typescript/config/config.default.ts
+++ examples/typescript/config/config.default.ts
@@ -1,10 +1,18 @@
 'use strict';
 
 import * as path from 'path';
-import { EggAppConfig, PowerPartial } from 'beidou';
+import { EggAppConfig, PowerPartial, EggAppInfo } from 'beidou';
 
-export default (): any => {
-  const config = {} as PowerPartial<EggAppConfig>;
+// 应用本身的配置 Scheme
+export interface StaticConfig {
+  static: {
+    dir: ({ prefix: string, dir: string })[]
+  };
+}
+
+
+export default (appInfo: EggAppInfo): any => {
+  const config = {} as PowerPartial<EggAppConfig & StaticConfig>;
 
   config.keys = 'secret';
 
@@ -20,7 +28,7 @@ export default (): any => {
   config.isomorphic = {
     // babel: false,
   };
-  
+
   config.webpack = {
     // your webpack config file
     custom: {
@@ -32,6 +40,19 @@ export default (): any => {
         client: path.join(__dirname, '../client'),
       },
     },
+  }
+
+  config.static = {
+    dir: [
+      {
+        prefix: '/public',
+        dir: path.join(appInfo.baseDir, '/app/public'),
+      },
+      {
+        prefix: '/build',
+        dir: path.join(appInfo.baseDir, '/build'),
+      },
+    ],
   }
   return config;
 };
