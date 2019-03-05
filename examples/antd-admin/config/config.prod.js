3cd128e83c2d650d8e14e19f1311348f664f904c
Gray <njugray@gmail.com>
2019-03-05 17:20:57 +0800

    example: upgrade

--- examples/antd-admin/config/config.prod.js
+++ examples/antd-admin/config/config.prod.js
@@ -1,3 +1,11 @@
 'use strict';
 
-module.exports = {};
+const path = require('path');
+
+module.exports = {
+  static: {
+    prefix: '/build',
+    dir: path.join(__dirname, '../build'),
+  },
+};
+
