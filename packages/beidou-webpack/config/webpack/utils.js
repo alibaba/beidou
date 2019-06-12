1f581bd80d7d3b94245c29b7b17e6ca8282593ca
justquanyin <898617222@163.com>
2019-06-12 16:06:04 +0800

    feat: remove browsers config in autoprefixer v9.6.0

--- packages/beidou-webpack/config/webpack/utils.js
+++ packages/beidou-webpack/config/webpack/utils.js
@@ -53,7 +53,8 @@ const postCssLoaderConfig = {
     plugins: () => [
       require('postcss-flexbugs-fixes'),
       autoprefixer({
-        browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
+        // browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
+        // please set browserslist in package.json
         flexbox: 'no-2009',
       }),
     ],
